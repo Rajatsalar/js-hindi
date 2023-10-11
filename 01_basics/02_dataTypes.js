a80c6213c1c6c851ff9f7bd22d9a62cac2425ac0
Rajatsalar <147129138+Rajatsalar@users.noreply.github.com>
2023-10-11 15:24:45 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+alert(3 + 3)// we are using nodejs, not browser
+
+let name = "Rajat"
+let age = 28
+let isLoggedIn = false
+
+// number => 2 to power 52
+// bigint
+// string => ""
+// boolean => true/false
+// null
+// undefined 
+// symbol => unique
+
+
+// object
