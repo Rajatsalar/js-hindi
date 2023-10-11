2a33ed9116686b7ffa938a1227cd4c72a716ab59
Rajatsalar <147129138+Rajatsalar@users.noreply.github.com>
2023-10-11 17:11:16 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,3 +12,19 @@
 //let booleanIsLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn);
 
+// ************************ Operations **************************
+
+//let value = 3
+//let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "Rajat"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2); => it will print 12
+//console.log(1 + "2"); => it will print 12
+//console.log("1" + (2 + 2)); it will print 14
+
