88a60dd7739f268c006ae73b5b4d369b0641e762
Rajatsalar <147129138+Rajatsalar@users.noreply.github.com>
2023-10-11 10:14:10 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "rajatdevsalan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bijnor"
+
+// accountId = 2 not allowed in js
+/*
+prefer not to use var bcse of issue in block scope and functional scope.
+/*
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Noida"
+
+console.table([accountId, accountEmail, accountPassword])
